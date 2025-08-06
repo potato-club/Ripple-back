d6b2769706cc51f070d7346d24ba9f3b8bcea79a
choigyuseong <clitico12@gmail.com>
2025-08-06 15:57:10 +0900

    feat: 기초 환경 설정

---
+++ src/test/java/org/example/rippleback/RippleBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.rippleback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RippleBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
