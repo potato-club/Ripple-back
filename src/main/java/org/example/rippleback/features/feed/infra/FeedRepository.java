60b30461e16975a42e165de8f908e1d398b62bfd
choigyuseong <clitico12@gmail.com>
2025-11-15 01:54:20 +0900

    refactor: post 사용하던거 feed 로 수정

--- src/main/java/org/example/rippleback/features/feed/infra/FeedRepository.java
+++ src/main/java/org/example/rippleback/features/feed/infra/FeedRepository.java
@@ -1,12 +1,15 @@
 package org.example.rippleback.features.feed.infra;
 
 import org.example.rippleback.features.feed.domain.Feed;
+import org.example.rippleback.features.feed.domain.FeedStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface FeedRepository extends JpaRepository<Feed, Long> {
+    long countByAuthorIdAndStatus(Long authorId, FeedStatus status);
+
     @Modifying(clearAutomatically = true)
     @Query("""
         update Feed f
